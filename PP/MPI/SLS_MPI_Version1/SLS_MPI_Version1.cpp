81139160ee4b2ac1b05edd69cbeb85b5b235662a
Kanadesss <n.tropin@g.nsu.ru>
2024-03-25 01:05:43 +0700

    testing base mpi things

---
+++ PP/MPI/SLS_MPI_Version1/SLS_MPI_Version1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "mpi.h"
+
+int main(int argc, char** argv)
+{
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    printf("Hello World! proc:%d\n", rank);
+    MPI_Finalize();
+    std::cout << "process: " << rank << " size: " << size << std::endl;
+    //std::cout << "End\n";
+}
