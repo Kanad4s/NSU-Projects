acf44ae5d242b86d145f4701eff911ba224c01ac
Kanadesss <n.tropin@g.nsu.ru>
2024-05-25 09:23:36 +0700

    uprgade zombiParent

--- OS/Task5/ThreadZombi/ThreadZombi.cpp
+++ OS/Task5/ThreadZombi/ThreadZombi.cpp
@@ -2,42 +2,52 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stddef.h>
 
 int global = 22;
-
-void printVars(int globalVar, int localVar) {
-	printf("global: %d, address = %p\n", globalVar, &globalVar);
-	printf("local: %d, address = %p\n", localVar, &localVar);
-}
 
 void createProc(int local) {
 	int status;
 	pid_t pid = fork();
+	pid_t pid2;
 	if (pid == 0) {
 		printf("Child: pid = %d\n", getpid());
-		printf("Child: parent pid = %d\n", getppid());
+		/*printf("Child: parent pid = %d\n", getppid());
 		printf("Child: global: %d, address = %p\n", global, &global);
-		printf("Child: local: %d, address = %p\n", local, &local);
+		printf("Child: local: %d, address = %p\n", local, &local);*/
 		local = 55555;
 		global = 88888;
-		printf("Child: change vars in child:\n");
+		/*printf("Child: change vars in child:\n");
 		printf("Child: global: %d, address = %p\n", global, &global);
-		printf("Child: local: %d, address = %p\n", local, &local);
-		sleep(10);
-		waitpid(getppid(), &status, 0);
-		printf("Child: exit code of parent = %d\n", WEXITSTATUS(status));
-		sleep(10);
+		printf("Child: local: %d, address = %p\n", local, &local);*/
+		sleep(5);
+		printf("second fork\n");
+		pid2 = fork();
+		if (pid2 == 0) {
+			printf("Grandchild, pid: %d\n", getpid());
+			sleep(10);
+			exit(10);
+		}
+		else if (pid2 > 0) {
+			printf("Grandchild`s parent, pid: %d\n", getpid());
+			sleep(5);
+			exit(10);
+		}
+		//waitpid(getppid(), &status, 0);
+		//printf("Child: exit code of parent = %d\n", WEXITSTATUS(status));
+		//sleep(10);
 		return;
 	}
 	else if (pid > 0) {
-		printf("Parent: global: %d, address = %p\n", global, &global);
-		printf("Parent: local: %d, address = %p\n", local, &local);
-		sleep(5);
+		/*printf("Parent: global: %d, address = %p\n", global, &global);
+		printf("Parent: local: %d, address = %p\n", local, &local);*/
+		sleep(50);
 		//sleep(100);
 		//waitpid(pid, &status, 0);
 		//wait(&tmp);
 		// parent process waits until the end of execution of child process
 		//printf("Parent: exit code of child = %d\n", WEXITSTATUS(status));
+		sleep(30);
 		exit(5);
 	}
 	else {
@@ -48,9 +58,9 @@ void createProc(int local) {
 
 int main() {
 	int local = 11;
-	printf("In function main:\n");
+	/*printf("In function main:\n");
 	printf("global: %d, address = %p\n", global, &global);
-	printf("local: %d, address = %p\n", local, &local);
+	printf("local: %d, address = %p\n", local, &local);*/
 	printf("Main parent pid = %d\n", getpid());
 	printf("Main parent parent pid = %d\n", getppid());
 	sleep(5);
