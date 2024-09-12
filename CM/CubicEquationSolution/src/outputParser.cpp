e72b3beb87dbdb4fa3bd0b6f260f89667da10098
Kanad4s <n.tropin@g.nsu.ru>
2024-09-12 16:34:48 +0700

    add print roots

--- CM/CubicEquationSolution/src/outputParser.cpp
+++ CM/CubicEquationSolution/src/outputParser.cpp
@@ -1,5 +1,10 @@
 #include "outputParser.h"
+#include <iostream>
 
-void outputParser::showResults(std::vector<int> solution) {
+void outputParser::showResults(std::vector<double> solution) {
+    std::cout << "roots found: " << solution.size() << std::endl;
+    for (int i = 0; i < solution.size(); i++) {
+        std::cout << "root № " << i + 1 << ": " << solution.at(i) << std::endl;
+    }
 
 }
