efb060b16a01892b40b29906ae4412b86edb964d
Kanad4s <n.tropin@g.nsu.ru>
2024-09-17 17:25:12 +0700

    parse args by inputParser

--- CM/CubicEquationSolution/src/inputParser.cpp
+++ CM/CubicEquationSolution/src/inputParser.cpp
@@ -4,8 +4,19 @@
 
 bool inputParser::parseInput(int argc, char *argv[], double *a, double *b, double *c, double *d, double* accuracy, double* step)
 {
-   
-    std::cout << argc << std::endl;
+    std::cout << "arguments count: " << argc << "arguments:" << std::endl;
+    for (int i = 0; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
+    if (argc == 6) {
+        solver::B = atof(argv[1]);
+        solver::C = atof(argv[2]);
+        solver::D = atof(argv[3]);
+        solver::STEP = atof(argv[4]);
+        solver::ACCURACY = atof(argv[5]);
+    } else {
+        std::cout << "usage: ces-prog <B> <C> <D> <STEP> <ACCURACY>" << std::endl;
+    }
     *a = solver::A;
     *b = solver::B;
     *c = solver::C;
