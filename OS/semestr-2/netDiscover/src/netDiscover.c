0115cee32049e17969fd026c0dd48e4dec33baf5
Kanad4s <n.tropin@g.nsu.ru>
2024-10-01 17:22:32 +0700

    store semestr-1 tasks in one repositoru

--- OS/semestr-2/netDiscover/src/netDiscover.c
+++ OS/semestr-2/netDiscover/src/netDiscover.c
@@ -7,6 +7,10 @@
 #include <signal.h>
 #include <unistd.h>
 
+#include <features.h>
+#include <bits/stdint-uintn.h>
+#include <bits/types.h>
+
 #include <sys/cdefs.h>
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -16,17 +20,46 @@
 
 #include "../include/inputParser.h"
 
-int create_multicast_socket(int* socket, const char* port, const char* ip) {
+typedef enum Result {
+    ERROR,
+    OK
+};
+
+enum Result create_multicast_socket(int* socket, const char* port, const char* ip) {
+    int err;
     const int optval = 1;
     setsockopt(*socket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+    struct in_addr ipToNum;
     struct sockaddr_in addr;
     bzero(&addr, sizeof(addr));
+    err = inet_pton(AF_INET, ip, &ipToNum);
+    if (err <= 0) {
+        printf("Error in IP translation to special numeric format\n");
+        goto error;
+    }
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    // addr.sin_addr.s_addr = htonl()
-    // struct addrinfo addrinfo1;
-    
+    addr.sin_port = htons(atoi(port));
+    addr.sin_addr = ipToNum;
+    err = bind(socket, (struct sockaddr *)&addr, sizeof(addr));
+    if (err != 0) {
+        printf("bind(): %s", strerror(err));
+        goto error;
+    }
 
+    return OK;
+
+error:
+    close(socket);
+    return ERROR;
+
+
+    struct addrinfo* hints;
+    struct addrinfo* result;
+    err = getaddrinfo(ip, port, hints, &result);
+    if (err != 0) {
+        printf("getaddrinfo(): %s", gai_strerror(err));
+        return ERROR;
+    }
 }
 
 int main(int argc, char* argv[]) {
