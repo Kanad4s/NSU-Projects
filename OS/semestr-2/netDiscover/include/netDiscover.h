e239c900845452ac5b920c8875d4e59555768dea
Kanad4s <n.tropin@g.nsu.ru>
2024-10-20 20:54:08 +0700

    add printCopies

--- OS/semestr-2/netDiscover/include/netDiscover.h
+++ OS/semestr-2/netDiscover/include/netDiscover.h
@@ -20,5 +20,7 @@ Result multicastAddMembership(int sockfd, int addrFamily, struct sockaddr_storag
 Result createMulticastSocket(int* sockfd, const char* port, const char* ip, struct sockaddr_storage *groupAddr, socklen_t *addrLen);
 Result sendMessage(int sockfd, int *msg, struct sockaddr *destAddr, socklen_t *addrLen);
 Result recieveMessage(int sockfd, int *msg, struct sockaddr *srcAddr, socklen_t *addrlen);
+Result printAppCopies(int sockfd);
+Result printAppInfo(struct sockaddr *addr, socklen_t addrlen);
 
 #endif
