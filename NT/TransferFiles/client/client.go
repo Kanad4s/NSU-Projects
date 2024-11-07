fdd3d951ea6c38a8d0da270f8de74bc8ba9d43ff
Kanad4s <n.tropin@g.nsu.ru>
2024-11-07 11:44:14 +0700

    clear code

--- NT/TransferFiles/client/client.go
+++ NT/TransferFiles/client/client.go
@@ -24,93 +24,40 @@ func main() {
 	}
 	defer conn.Close()
 
-	if !TestSend(conn) {
-		os.Exit(1)
+	if !PrepareSendFile(conn) {
+		os.Exit(0)
 	}
-	// if !PrepareSendFile(conn) {
-	// 	os.Exit(0)
-	// }
 
-}
-
-func TestSend(conn net.Conn) bool {
-	for i := 0; i < 2; i++ {
-		text := inputParser.GetFile()
-		if i == 1 {
-			text = "0"
-			if inputParser.IsOverwrite() {
-				text = "1"
-			}
-		}
-		conn.Write([]byte(text + string(endSymbol)))
-
-		// получаем ответ от сервера
-		response, err := bufio.NewReader(conn).ReadString(endSymbol)
-		if err != nil {
-			fmt.Println("Error reading:", err.Error())
-			os.Exit(1)
-		}
-		fmt.Printf("Server says: %s\n", response)
-	}
-	response, err := bufio.NewReader(conn).ReadString(endSymbol)
-	if err != nil {
-		fmt.Println("Error reading:", err.Error())
-		os.Exit(1)
-	}
-	fmt.Printf("Server says: %s\n", response)
-
-	// doSend := DoSendFile(conn)
-	doSend := true
-	return doSend
 }
 
 func PrepareSendFile(conn net.Conn) bool {
 	SendFileName(conn, inputParser.GetFile())
 	SendOverwrite(conn, inputParser.IsOverwrite())
-	fmt.Println("send done")
+	fmt.Println("send parameters done")
 	doSend := DoSendFile(conn)
 	return doSend
 }
 
 func PrintMessage(message string) {
 	if message == "9876" {
-		fmt.Println("Sending is accepted")
+		fmt.Println("File sending is accepted")
 	} else if message == "ABCD" {
-		fmt.Println("Sending is unaccepted")
+		fmt.Println("File sending is unaccepted")
 	}
 }
 
 func DoSendFile(conn net.Conn) bool {
-	store := make([]byte, 0, 4096)
-
 	response := GetMessage(conn)
-	// response, err := bufio.NewReader(conn).ReadString(endSymbol)
-	// if err != nil {
-	// 	fmt.Println("Error reading:", err.Error())
-	// 	os.Exit(1)
-	// }
-	fmt.Println("response: ", response)
-	fmt.Printf("get message: %v, size: %v\n", store, len(store))
-	message := "zaglushka"
-	fmt.Println("get message: ", message)
-
-	PrintMessage(message)
-	return message == "9876\n"
+	PrintMessage(response)
+	return response == "9876"
 }
 
 func SendFileName(conn net.Conn, name string) {
-	// writer := bufio.NewWriter(conn)
-	// n, err := writer.Write([]byte(name + "\n"))
-	// n, err := conn.Write([]byte(name + string(endSymbol)))
 	SendMessage(name, conn)
 	response := GetMessage(conn)
 	if response != successMsg {
 		os.Exit(1)
 	}
-	// if err != nil {
-	// 	fmt.Println("error send FileName:", err.Error())
-	// 	os.Exit(1)
-	// }
 }
 
 func SendOverwrite(conn net.Conn, isOverwrite bool) {
@@ -118,19 +65,11 @@ func SendOverwrite(conn net.Conn, isOverwrite bool) {
 	if isOverwrite {
 		sendValue = "1"
 	}
-	// writer := bufio.NewWriter(conn)
-	// n, err := writer.Write([]byte(sendValue + "\n"))
-	// n, err := conn.Write([]byte(sendValue + string(endSymbol)))
 	SendMessage(sendValue, conn)
 	response := GetMessage(conn)
 	if response != successMsg {
 		os.Exit(1)
 	}
-	// fmt.Println("overwrite write(): ", n)
-	// if err != nil {
-	// 	fmt.Println("error send Overwrite:", err.Error())
-	// 	os.Exit(1)
-	// }
 }
 
 func GetMessage(conn net.Conn) (msg string) {
