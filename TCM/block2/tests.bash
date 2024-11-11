5972785493461a0720036ab9ab9d197ad1fe094a
Kanad4s <n.tropin@g.nsu.ru>
2024-11-11 22:34:41 +0700

    add Pow

--- TCM/block2/tests.bash
+++ TCM/block2/tests.bash
@@ -8,9 +8,14 @@ start:
     rpn:
 flowcpp PEG.flow -- exp="(((11 3 -) (2 4 ^) /) ((0 (20 2 /) -) 5 +) *)" -- mode=rpn
 flowcpp PEG.flow -- exp="((8 (2 4 ^) /) ((5 2000 *) (3 (2 (3 2 ^) ^) *) -) -)" -- mode=rpn 
-    simplify:
+    simplifyRational:
 flowcpp PEG.flow -- exp="((1 / x) + ((2 / y) + (3 / z)))" -- mode=rat
 flowcpp PEG.flow -- exp="((9-a)*(8+a-b))" -- mode=rat
 flowcpp PEG.flow -- exp="((((a+1)/(7*c))*((7+(6*c))/x)) / ((3*(a+1))/(x*x)))" -- mode=rat
 flowcpp PEG.flow -- exp="((1/1000000000)+(1/1000000000))" -- mode=rat
 flowcpp PEG.flow -- exp="(((4*(a*a))-9)*((1/((2*a)-3))-(1/((2*a)+3))))" -- mode=rat
+    dif:
+    sim:
+flowcpp PEG.flow -- exp="(((3^1)-(2*0))+(6-0))" -- mode=sim
+
+
