07d52df31bed33528fee0fb31eee5d2075693b73
Kanad4s <n.tropin@g.nsu.ru>
2024-11-12 14:58:49 +0700

    add Simplifying

--- TCM/block2/tests.bash
+++ TCM/block2/tests.bash
@@ -15,6 +15,7 @@ flowcpp PEG.flow -- exp="((((a+1)/(7*c))*((7+(6*c))/x)) / ((3*(a+1))/(x*x)))" --
 flowcpp PEG.flow -- exp="((1/1000000000)+(1/1000000000))" -- mode=rat
 flowcpp PEG.flow -- exp="(((4*(a*a))-9)*((1/((2*a)-3))-(1/((2*a)+3))))" -- mode=rat
     dif:
+flowcpp PEG.flow -- exp="((y / x) - (z + 1))" -- mode=dif
     sim:
 flowcpp PEG.flow -- exp="(((3^1)-(2*0))+(6-0))" -- mode=sim
 
