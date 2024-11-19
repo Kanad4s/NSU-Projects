6986d85bdab75a11d7144064f0d550859a0be6ae
Kanad4s <n.tropin@g.nsu.ru>
2024-11-19 15:52:48 +0700

    add forming equation mode

--- CM/TridiagonalSLAE/cmd/main.go
+++ CM/TridiagonalSLAE/cmd/main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	inputParser.ParseInput()
-	n, eps, gamma := inputParser.GetInput()
-	fmt.Printf("%v, %v, %v\n", n, eps, gamma)
+	n, eps, gamma, mode := inputParser.GetInput()
+	fmt.Printf("%v, %v, %v, %v\n", n, eps, gamma, mode)
 }
--- CM/TridiagonalSLAE/internal/inputParser/inputParser.go
+++ CM/TridiagonalSLAE/internal/inputParser/inputParser.go
@@ -21,18 +21,31 @@ func ParseInput() {
 	}
 }
 
-func GetInput() (n int, eps float64, gamma float64) {
+func GetInput() (n int, eps float64, gamma float64, mode int) {
 	n, err := strconv.Atoi(args.N)
 	if err != nil {
 		fmt.Println("Error converting N to int")
 	}
-	eps, err = strconv.ParseFloat(args.Eps, 64)
-	if err != nil {
-		fmt.Println("Error converting Eps to int")
-	}
-	gamma, err = strconv.ParseFloat(args.Gamma, 64)
-	if err != nil {
-		fmt.Println("Error converting Gamma to int")
+	mode = 0
+	if (args.Eps != "" || args.Gamma != "") {
+		if args.Eps != "" {
+			eps, err = strconv.ParseFloat(args.Eps, 64)
+			if err != nil {
+				fmt.Println("Error converting Eps to float64")
+			}
+			mode = 1
+		}
+		if args.Gamma != "" {
+			gamma, err = strconv.ParseFloat(args.Gamma, 64)
+			if err != nil {
+				fmt.Println("Error converting Gamma to float64")
+			}
+			if mode == 1 {
+				mode = 3
+			} else {
+				mode = 2
+			}
+		}
 	}
 	return
 }
