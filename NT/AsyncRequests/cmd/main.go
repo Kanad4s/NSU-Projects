65622b2552e271c41b08b4400d750129c620b31c
Kanad4s <n.tropin@g.nsu.ru>
2024-12-12 01:49:37 +0700

    add city selecting

--- NT/AsyncRequests/cmd/main.go
+++ NT/AsyncRequests/cmd/main.go
@@ -12,24 +12,26 @@ import (
 
 func main() {
 	mistralKey, request := cli.Parse()
-	
+
 	var wg sync.WaitGroup
 	if len(request) > 0 {
 		wg.Add(1)
 		go mistral.MistralRequest(request, &wg, mistralKey)
 	}
 
-	fmt.Println("Scanning for interesting places")
-	places := places.Request()
+	city := cli.ChooseCity()
+	fmt.Printf("Scanning for interesting places in %s\n", city.Name)
+	places := places.RequestPlaces(city)
+	places.Print()
 	placeId := cli.ChoosePlace(len(places.Results))
 	fmt.Printf("Selected place № %d, %s\n", placeId, places.Results[placeId].Title)
 
 	request = mistral.BuildPlaceRequest(places.Results[placeId].Title, places.Results[placeId].Address)
 
 	wg.Add(1)
-	weather.Request(places.Results[placeId].Coords, &wg)
-	wg.Add(1)
 	go mistral.MistralRequest(request, &wg, mistralKey)
+	wg.Add(1)
+	go weather.Request(places.Results[placeId].Coords, &wg)
 
 	wg.Wait()
 }
