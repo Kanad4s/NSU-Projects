53c7d443c690e5a96457072afe86e37a9d7ed833
Kanad4s <n.tropin@g.nsu.ru>
2025-03-24 16:53:06 +0700

    add manufactured products

--- DB/Client-server/db/ddl/products.sql
+++ DB/Client-server/db/ddl/products.sql
@@ -59,6 +59,12 @@ CREATE TABLE IF NOT EXISTS "Дельтапланы" (
 	"угол_веера" NUMERIC(5,2)  NOT NULL CHECK ("угол_веера" BETWEEN 0 AND 180)
 );
 
+CREATE TABLE IF NOT EXISTS "Выпускаемые_изделия" (
+    "id" SERIAL PRIMARY KEY,
+    "модель" INTEGER NOT NULL REFERENCES "Модели_изделий"("id") ON DELETE CASCADE ON UPDATE RESTRICT,
+    "начало_производства" DATE NOT NULL CHECK ("начало_производства" <= CURRENT_DATE),
+    "дата_выпуска" DATE CHECK ("дата_выпуска" >= "начало_производства" OR "дата_выпуска" IS NULL)
+);
 
 
 
