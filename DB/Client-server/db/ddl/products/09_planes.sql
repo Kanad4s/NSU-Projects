c2adde38fdaf965f1a548e680706520350cafd94
Kanad4s <n.tropin@g.nsu.ru>
2025-03-26 10:11:08 +0700

    update references

--- DB/Client-server/db/ddl/products/09_planes.sql
+++ DB/Client-server/db/ddl/products/09_planes.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "Самолеты" (
     "id" INTEGER NOT NULL UNIQUE REFERENCES "Модели_изделий"("id") ON DELETE CASCADE ON UPDATE RESTRICT,
-    "категория" INTEGER NOT NULL REFERENCES "Категории_самолетов"("id") ON DELETE CASCADE ON UPDATE RESTRICT,
+    "категория" INTEGER NOT NULL REFERENCES "Категории_самолетов"("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     "размах_крыла" NUMERIC(6, 2) NOT NULL CHECK ("размах_крыла" > 0),
     "кол-во_мест" INTEGER NOT NULL CHECK ("кол-во_мест" > 0),
     "макс_скорость" INTEGER NOT NULL CHECK ("макс_скорость" BETWEEN 0 AND 300000000)
--- DB/Client-server/db/ddl/staff/04_workers.sql
+++ DB/Client-server/db/ddl/staff/04_workers.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS "Рабочие" (
     "id" serial PRIMARY KEY,
-    "человек" INTEGER NOT NULL UNIQUE REFERENCES "Люди"("id") ON DELETE CASCADE ON UPDATE RESTRICT,
+    "человек" INTEGER NOT NULL UNIQUE REFERENCES "Люди"("id") ON DELETE CASCADE ON UPDATE CASCADE,
     "бригада" INTEGER,
-    "категория" INTEGER NOT NULL REFERENCES "Категории_рабочих"("id") ON DELETE CASCADE ON UPDATE RESTRICT,
+    "категория" INTEGER NOT NULL REFERENCES "Категории_рабочих"("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     "физическая_форма" varchar(255) NOT NULL CHECK ("физическая_форма" IN ('отличная', 'хорошая', 'удовлетворительная', 'плохая')),
     "размер_спецодежды" INTEGER NOT NULL CHECK ("размер_спецодежды" BETWEEN 40 AND 60)
 );
