e0646bd9223ae8a18213f729e92e9f7eb2d93d6d
Kanad4s <n.tropin@g.nsu.ru>
2025-04-02 11:12:49 +0700

    add workshops and products up except manufactured products

--- DB/Client-server/db/ddl/workshops/20_assemblyPlans.sql
+++ DB/Client-server/db/ddl/workshops/20_assemblyPlans.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS "План_сборки" (
 	"id" SERIAL PRIMARY KEY,
 	"модель" INTEGER NOT NULL REFERENCES "Модели_изделий"("id") ON DELETE CASCADE ON UPDATE CASCADE,
-	"работа" INTEGER NOT NULL REFERENCES "Виды_работ_на_участке"("id") ON DELETE RESTRICT ON UPDATE RESTRICT
+	"работа" INTEGER NOT NULL REFERENCES "Виды_работ_на_участке"("id") ON DELETE RESTRICT ON UPDATE RESTRICT,
+	"этап" INTEGER NOT NULL UNIQUE CHECK ("этап" > 0),
+	UNIQUE("модель", "работа")
 );
