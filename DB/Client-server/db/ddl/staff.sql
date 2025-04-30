1a5ba152b0d25dbc7541310edff211789ab3cad5
Kanad4s <n.tropin@g.nsu.ru>
2025-04-30 15:43:33 +0700

    add staff triggers

--- DB/Client-server/db/ddl/staff.sql
+++ DB/Client-server/db/ddl/staff.sql
@@ -45,9 +45,64 @@ CREATE TABLE IF NOT EXISTS "Рабочие" (
     "размер_спецодежды" INTEGER NOT NULL CHECK ("размер_спецодежды" BETWEEN 40 AND 60)
 );
 
+CREATE OR REPLACE FUNCTION check_person_unique()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_TABLE_NAME = 'Рабочие' THEN
+        IF EXISTS (SELECT 1 FROM "ИТ_персонал" WHERE "человек" = NEW."человек") THEN
+            RAISE EXCEPTION 'Person with id % already exists in it table', NEW."человек";
+        END IF;
+    END IF;
+    
+    IF TG_TABLE_NAME = 'ИТ_персонал' THEN
+        IF EXISTS (SELECT 1 FROM "Рабочие" WHERE "человек" = NEW."человек") THEN
+            RAISE EXCEPTION 'Person with id % already exists in workers table', NEW."человек";
+        END IF;
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER check_worker_unique
+BEFORE INSERT OR UPDATE ON "Рабочие"
+FOR EACH ROW EXECUTE FUNCTION check_person_unique();
+
+CREATE OR REPLACE TRIGGER check_it_unique
+BEFORE INSERT OR UPDATE ON "ИТ_персонал"
+FOR EACH ROW EXECUTE FUNCTION check_person_unique();
+
 CREATE TABLE IF NOT EXISTS "Бригады" (
 	"id" SERIAL PRIMARY KEY,
 	"бригадир" INTEGER NOT NULL UNIQUE REFERENCES "Рабочие"("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 ALTER TABLE "Рабочие" ADD CONSTRAINT "Рабочие_fk2" FOREIGN KEY ("бригада") REFERENCES "Бригады"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+CREATE OR REPLACE FUNCTION check_brigadir_consistency()
+RETURNS trigger AS $$
+DECLARE
+    brigade_id INTEGER;
+BEGIN
+    SELECT id INTO brigade_id
+    FROM "Бригады"
+    WHERE "бригадир" = NEW.id;
+
+    IF brigade_id IS NOT NULL THEN
+        IF NEW.бригада IS DISTINCT FROM brigade_id THEN
+            RAISE EXCEPTION 'Рабочий (id=%) является бригадиром бригады (id=%), но указан в другой бригаде (id=%).',
+                NEW.id, brigade_id, NEW.бригада;
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trg_check_brigadir_consistency
+BEFORE INSERT OR UPDATE ON "Рабочие"
+FOR EACH ROW
+EXECUTE FUNCTION check_brigadir_consistency();
+
+
+
 
