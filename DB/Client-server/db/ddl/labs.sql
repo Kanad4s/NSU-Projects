b2f1fa8bbe206108647bfd7139ca7a12a1d240ea
Kanad4s <n.tropin@g.nsu.ru>
2025-06-04 00:27:32 +0700

    add tests

--- DB/Client-server/db/ddl/labs.sql
+++ DB/Client-server/db/ddl/labs.sql
@@ -20,6 +20,9 @@ CREATE TABLE IF NOT EXISTS "Испытатели" (
 	"лаборатория" INTEGER NOT NULL REFERENCES "Лаборатории"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+
+
+
 CREATE TABLE IF NOT EXISTS "Набор_испытаний" (
 	"id" SERIAL PRIMARY KEY,
 	"модель" INTEGER NOT NULL REFERENCES "Модели_изделий"("id") ON DELETE CASCADE ON UPDATE CASCADE,
@@ -44,4 +47,40 @@ CREATE TABLE IF NOT EXISTS "Испытания" (
 	"испытатель" INTEGER NOT NULL REFERENCES "Испытатели"("id") ON DELETE RESTRICT ON UPDATE CASCADE,
 	"оборудование" INTEGER NOT NULL REFERENCES "Испытательное_оборудование"("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+
+CREATE OR REPLACE FUNCTION проверить_дату_работы_испытателя()
+RETURNS TRIGGER AS $$
+DECLARE
+    дата DATE := NEW."дата";
+    дата_устройства DATE;
+    дата_увольнения DATE;
+BEGIN
+    SELECT л."дата_устройства", л."дата_увольнения"
+    INTO дата_устройства, дата_увольнения
+    FROM "Люди" л
+    JOIN "ИТ_персонал" ит ON ит."id" = (
+        SELECT "id"
+        FROM "Испытатели"
+        WHERE "id" = NEW."испытатель"
+    )
+    WHERE л."id" = ит."человек";
+
+    IF дата < дата_устройства THEN
+        RAISE EXCEPTION 'Дата испытания (%), раньше даты устройства испытателя (%)', дата, дата_устройства;
+    END IF;
+
+    IF дата_увольнения IS NOT NULL AND дата >= дата_увольнения THEN
+        RAISE EXCEPTION 'Дата испытания (%), позже даты увольнения испытателя (%)', дата, дата_увольнения;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER триггер_проверка_даты_испытателя
+BEFORE INSERT OR UPDATE ON "Испытания"
+FOR EACH ROW
+EXECUTE FUNCTION проверить_дату_работы_испытателя();
+
 
