722d007aa675cff5ad198ac5116d2a33a9586d17
Kanad4s <n.tropin@g.nsu.ru>
2025-06-06 23:58:01 +0700

    add app package

--- DB/Client-server/cmd/main.go
+++ DB/Client-server/cmd/main.go
@@ -1,14 +1,8 @@
 package main
 
 import (
+	"client-server/internal/app"
 	"client-server/internal/db"
-	"client-server/internal/handler/functions"
-	"log"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html/v2"
-
-	"client-server/internal/route"
 )
 
 func main() {
@@ -24,24 +18,5 @@ func main() {
 	db.CreateTables(database)
 	db.FillData(database)
 
-	engine := html.New("../views", ".html")
-	engine.Reload(true)
-	engine.AddFunc("FormatDate", functions.FormatDate)
-	engine.AddFunc("FormatDateWithNull", functions.FormatDateWithNull)
-	app := fiber.New(fiber.Config{
-		Views: engine,
-		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			referer := c.Get("Referer", "/")
-			return c.Status(fiber.StatusInternalServerError).Render("error", fiber.Map{
-				"Message": err.Error(),
-				"BackURL": referer,
-			})
-		},
-	})
-	app.Static("/img", "../src/img")
-	app.Static("/static", "../static")
-
-	route.Setup(app, database)
-
-	log.Fatal(app.Listen(":3003"))
+	app.Start(database)
 }
---
+++ DB/Client-server/internal/app/app.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	"client-server/internal/handler/functions"
+	"client-server/internal/route"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+	"github.com/jmoiron/sqlx"
+)
+
+func Start(database *sqlx.DB) {
+	app := fiber.New(fiber.Config{
+		Views: setupEngine(),
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			referer := c.Get("Referer", "/")
+			return c.Status(fiber.StatusInternalServerError).Render("error", fiber.Map{
+				"Message": err.Error(),
+				"BackURL": referer,
+			})
+		},
+	})
+	app.Static("/img", "../src/img")
+	app.Static("/static", "../static")
+
+	route.Setup(app, database)
+
+	log.Fatal(app.Listen(":3003"))
+}
+
+func setupEngine() *html.Engine {
+	engine := html.New("../views", ".html")
+	engine.Reload(true)
+	engine.AddFunc("FormatDate", functions.FormatDate)
+	engine.AddFunc("FormatDateWithNull", functions.FormatDateWithNull)
+	return engine
+}
