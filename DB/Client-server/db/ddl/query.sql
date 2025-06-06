ef0f347deb3caa95ef0acd84378f5a74a4c64ba4
Kanad4s <n.tropin@g.nsu.ru>
2025-06-06 23:01:38 +0700

    finish docx

--- DB/Client-server/db/ddl/query.sql
+++ DB/Client-server/db/ddl/query.sql
@@ -142,36 +142,36 @@ $$ LANGUAGE plpgsql;
 
 -- SELECT * FROM кадровый_состав();
 
--- CREATE OR REPLACE FUNCTION перечень_участков(
---     цех_id INTEGER DEFAULT NULL
--- )
--- RETURNS TABLE (
---     id_участка INTEGER,
---     начальник_id INTEGER,
---     начальник_ФИО TEXT,
---     цех INTEGER,
---     количество_участков BIGINT
--- ) AS $$
--- BEGIN
---     RETURN QUERY
---     SELECT
---         у."id",
---         у."начальник",
---         п."ФИО"::TEXT,
---         у."цех",
---         COUNT(*) OVER (PARTITION BY у."цех") AS количество_участков
---     FROM "Участки_цехов" у
---     JOIN "ИТ_персонал" ит ON ит."id" = у."начальник"
---     JOIN "Люди" п ON п."id" = ит."id"
---     WHERE (цех_id IS NULL OR у."цех" = цех_id);
--- END;
--- $$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION перечень_участков(
+    цех_id INTEGER DEFAULT NULL
+)
+RETURNS TABLE (
+    id_участка INTEGER,
+    начальник_id INTEGER,
+    начальник_ФИО TEXT,
+    цех INTEGER,
+    количество_участков BIGINT
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        у."id",
+        у."начальник",
+        п."ФИО"::TEXT,
+        у."цех",
+        COUNT(*) OVER (PARTITION BY у."цех") AS количество_участков
+    FROM "Участки_цехов" у
+    JOIN "ИТ_персонал" ит ON ит."id" = у."начальник"
+    JOIN "Люди" п ON п."id" = ит."id"
+    WHERE (цех_id IS NULL OR у."цех" = цех_id);
+END;
+$$ LANGUAGE plpgsql;
 
 
 
 
 
--- -- SELECT * FROM перечень_участков(2);
+-- SELECT * FROM перечень_участков(2);
 
 CREATE OR REPLACE FUNCTION перечень_работ_изделия(
     изделие_id INTEGER
@@ -297,4 +297,6 @@ $$ LANGUAGE plpgsql;
 
 
 -- SELECT * FROM текущие_изделия();
+
+
 
--- DB/Client-server/db/ddl/query/4.areas.sql
+++ DB/Client-server/db/ddl/query/4.areas.sql
@@ -1,30 +1,30 @@
--- CREATE OR REPLACE FUNCTION перечень_участков(
---     цех_id INTEGER DEFAULT NULL
--- )
--- RETURNS TABLE (
---     id_участка INTEGER,
---     начальник_id INTEGER,
---     начальник_ФИО TEXT,
---     цех INTEGER,
---     количество_участков BIGINT
--- ) AS $$
--- BEGIN
---     RETURN QUERY
---     SELECT
---         у."id",
---         у."начальник",
---         п."ФИО"::TEXT,
---         у."цех",
---         COUNT(*) OVER (PARTITION BY у."цех") AS количество_участков
---     FROM "Участки_цехов" у
---     JOIN "ИТ_персонал" ит ON ит."id" = у."начальник"
---     JOIN "Люди" п ON п."id" = ит."id"
---     WHERE (цех_id IS NULL OR у."цех" = цех_id);
--- END;
--- $$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION перечень_участков(
+    цех_id INTEGER DEFAULT NULL
+)
+RETURNS TABLE (
+    id_участка INTEGER,
+    начальник_id INTEGER,
+    начальник_ФИО TEXT,
+    цех INTEGER,
+    количество_участков BIGINT
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        у."id",
+        у."начальник",
+        п."ФИО"::TEXT,
+        у."цех",
+        COUNT(*) OVER (PARTITION BY у."цех") AS количество_участков
+    FROM "Участки_цехов" у
+    JOIN "ИТ_персонал" ит ON ит."id" = у."начальник"
+    JOIN "Люди" п ON п."id" = ит."id"
+    WHERE (цех_id IS NULL OR у."цех" = цех_id);
+END;
+$$ LANGUAGE plpgsql;
 
 
 
 
 
--- -- SELECT * FROM перечень_участков(2);
+-- SELECT * FROM перечень_участков(2);
