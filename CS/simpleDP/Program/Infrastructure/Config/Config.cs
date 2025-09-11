6de6c58f77c2005a896226b956b3885757decacf
Kanad4s <n.tropin@g.nsu.ru>
2025-09-12 00:26:52 +0700

    fix config namespace

--- CS/simpleDP/Program/Infrastructure/Config/Config.cs
+++ CS/simpleDP/Program/Infrastructure/Config/Config.cs
@@ -1,14 +1,17 @@
-public class SimulationConfig
+namespace Program.Infrastructure.Config
 {
-    public int Steps { get; set; }
-    public int ThinkingMin { get; set; }
-    public int ThinkingMax { get; set; }
-    public int EatingMin { get; set; }
-    public int EatingMax { get; set; }
-}
+    public class SimulationConfig
+    {
+        public int Steps { get; set; }
+        public int ThinkingMin { get; set; }
+        public int ThinkingMax { get; set; }
+        public int EatingMin { get; set; }
+        public int EatingMax { get; set; }
+    }
 
-public class AppConfig
-{
-    public SimulationConfig Simulation { get; set; } = new();
-    public string PhilosophersFile { get; set; } = "Config/appsettings.json";
+    public class AppConfig
+    {
+        public SimulationConfig Simulation { get; set; } = new();
+        public string PhilosophersFile { get; set; } = "Config/appsettings.json";
+    }
 }
